4175ea7d6083dd54ce182e94e12c4842ff62cf5f
cappuch <mikusdevr@gmail.com>
2026-08-24 21:41:02 +0100

    decomp: match func_020063f8

--- src/system/exceptionscreen.cpp
+++ src/system/exceptionscreen.cpp
@@ -3,3 +3,15 @@
 extern "C" void func_02006438(s16* out, s16 a, s16 b) {
 	*out = a + b;
 }
+
+extern "C" void func_020063f8(s16* out, s16 a, s8* str) {
+	if (*str == 0) {
+		return;
+	}
+	do {
+		s8 c = *str;
+		func_02006438(out, a, c);
+		str++;
+		out++;
+	} while (*str != 0);
+}
