1fa6def9398b6a38e27ca494ec9065c10cf12bdf
cappuch <mikusdevr@gmail.com>
2026-08-24 22:14:16 +0100

    decomp: match System vblank setters (report +52, 79504->79556)

--- src/system/vblank.cpp
+++ src/system/vblank.cpp
@@ -1,1 +1,22 @@
 #include "vblank.hpp"
+
+namespace System {
+
+#pragma section autobss_3 begin
+	IrqFunction userVBlankFunction;
+	s32 minigamesVBlankCounter;
+#pragma section autobss_3 end
+
+	void setUserVBlankHandler(IrqFunction handler) {
+		userVBlankFunction = handler;
+	}
+
+	void removeUserVBlankHandler() {
+		userVBlankFunction = NULL;
+	}
+
+	void setMinigamesVBlankCounter(s32 value) {
+		minigamesVBlankCounter = value;
+	}
+
+}
