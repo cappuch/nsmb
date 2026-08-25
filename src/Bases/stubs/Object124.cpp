380b55179f7337a246d2b87c2e6e15539a63dcc0
cappuch <mikusdevr@gmail.com>
2026-08-25 03:48:37 +0100

    tiny2: match 4 fn in src/Bases/stubs/Object124.cpp (func_ov110_0218a39c, func_ov110_0218a3a0, func_ov110_0218a3a4, func_ov110_0218a3a8)

--- src/Bases/stubs/Object124.cpp
+++ src/Bases/stubs/Object124.cpp
@@ -7,3 +7,19 @@ void *Object124::create()
 
 // 0x0218a800
 ActorProfile Object124_Profile = { Object124::create, 124, 143, NULL /* TODO: 0x0218a678 */ };
+
+extern "C" void func_ov110_0218a39c() {
+
+}
+
+extern "C" void func_ov110_0218a3a0() {
+
+}
+
+extern "C" void func_ov110_0218a3a4() {
+
+}
+
+extern "C" void func_ov110_0218a3a8() {
+
+}
