cee6c7b8732547c55140934b950673e849856e11
cappuch <mikusdevr@gmail.com>
2026-08-25 03:30:38 +0100

    tiny: match 7 fn in src/Bases/stubs/Object113.cpp (Object113::_24, Object113::_25, Object113::_26, Object113::_27, Object113::_36, Object113::onDestroy, Object113::pendingDestroy)

--- src/Bases/stubs/Object113.cpp
+++ src/Bases/stubs/Object113.cpp
@@ -7,3 +7,31 @@ void *Object113::create()
 
 // 0x021413d0
 ActorProfile Object113_Profile = { Object113::create, 113, 131, NULL /* TODO: 0x02140d38 */ };
+
+s32 Object113::onDestroy() {
+	return 1;
+}
+
+void Object113::pendingDestroy() {
+
+}
+
+void Object113::_36() {
+
+}
+
+void Object113::_25() {
+
+}
+
+void Object113::_27() {
+
+}
+
+void Object113::_26() {
+
+}
+
+void Object113::_24() {
+
+}
--- src/Bases/stubs/Object113.hpp
+++ src/Bases/stubs/Object113.hpp
@@ -5,6 +5,13 @@
 // MainProfileTable slot 113  |  ov028  |  profile @ 0x021413d0
 class Object113 : public StageEntity {
 public:
+	void _24();
+	void _26();
+	void _27();
+	void _25();
+	void _36();
+	void pendingDestroy();
+	s32 onDestroy();
 	static void *create();
 	inline Object113() {};
 	inline ~Object113() {};
