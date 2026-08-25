4299c697377327b6058f77e11b62c6d5b11aa991
cappuch <mikusdevr@gmail.com>
2026-08-25 03:48:42 +0100

    tiny2: match 2 fn in src/Bases/stubs/Object308.cpp (func_ov009_020d7580, func_ov009_020d7584)

--- src/Bases/stubs/Object308.cpp
+++ src/Bases/stubs/Object308.cpp
@@ -7,3 +7,11 @@ void *Object308::create()
 
 // 0x020daea0
 ActorProfile Object308_Profile = { Object308::create, 308, 299, NULL /* TODO: 0x020d7340 */ };
+
+extern "C" void func_ov009_020d7580() {
+
+}
+
+extern "C" int func_ov009_020d7584() {
+	return 1;
+}
