c9d4614d12d488fd24556d42416bcdf4a24ae618
cappuch <mikusdevr@gmail.com>
2026-08-25 03:30:42 +0100

    tiny: match 2 fn in src/Bases/stubs/Object263.cpp (Object263::onDestroy, Object263::pendingDestroy)

--- src/Bases/stubs/Object263.cpp
+++ src/Bases/stubs/Object263.cpp
@@ -7,3 +7,11 @@ void *Object263::create()
 
 // 0x0218fc30
 ActorProfile Object263_Profile = { Object263::create, 263, 262, NULL /* TODO: 0x0218c88c */ };
+
+s32 Object263::onDestroy() {
+	return 1;
+}
+
+void Object263::pendingDestroy() {
+
+}
--- src/Bases/stubs/Object263.hpp
+++ src/Bases/stubs/Object263.hpp
@@ -5,6 +5,8 @@
 // MainProfileTable slot 263  |  ov118  |  profile @ 0x0218fc30
 class Object263 : public StageEntity {
 public:
+	void pendingDestroy();
+	s32 onDestroy();
 	u8 _pad0[0xc];
 
 	static void *create();
