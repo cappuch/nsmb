43ae13778b740cbab7354d9c84ed2551b4e225d4
cappuch <mikusdevr@gmail.com>
2026-08-25 03:30:38 +0100

    tiny: match 7 fn in src/Bases/stubs/Object128.cpp (Object128::_22, Object128::_24, Object128::_25, Object128::_26, Object128::_27, Object128::entityCollision, Object128::onDestroy)

--- src/Bases/stubs/Object128.cpp
+++ src/Bases/stubs/Object128.cpp
@@ -7,3 +7,31 @@ void *Object128::create()
 
 // 0x02133a88
 ActorProfile Object128_Profile = { Object128::create, 128, 146, NULL /* TODO: 0x021335d0 */ };
+
+s32 Object128::onDestroy() {
+	return 1;
+}
+
+void Object128::_22() {
+
+}
+
+void Object128::_24() {
+
+}
+
+void Object128::_25() {
+
+}
+
+void Object128::_26() {
+
+}
+
+void Object128::_27() {
+
+}
+
+void Object128::entityCollision() {
+
+}
--- src/Bases/stubs/Object128.hpp
+++ src/Bases/stubs/Object128.hpp
@@ -5,6 +5,13 @@
 // MainProfileTable slot 128  |  ov016  |  profile @ 0x02133a88
 class Object128 : public StageEntity {
 public:
+	void entityCollision();
+	void _27();
+	void _26();
+	void _25();
+	void _24();
+	void _22();
+	s32 onDestroy();
 	static void *create();
 	inline Object128() {};
 	inline ~Object128() {};
