c14f8f5575a7fd2c106daa13b3431954b7e29e89
cappuch <mikusdevr@gmail.com>
2026-08-25 03:30:40 +0100

    tiny: match 3 fn in src/Bases/stubs/Object214.cpp (Object214::_01, Object214::onHeapCreated, Object214::pendingDestroy)

--- src/Bases/stubs/Object214.cpp
+++ src/Bases/stubs/Object214.cpp
@@ -7,3 +7,15 @@ void *Object214::create()
 
 // 0x0218c638
 ActorProfile Object214_Profile = { Object214::create, 214, 276, NULL /* TODO: 0x0218b7e0 */ };
+
+bool Object214::_01() {
+	return 0;
+}
+
+void Object214::pendingDestroy() {
+
+}
+
+bool Object214::onHeapCreated() {
+	return 1;
+}
--- src/Bases/stubs/Object214.hpp
+++ src/Bases/stubs/Object214.hpp
@@ -5,6 +5,9 @@
 // MainProfileTable slot 214  |  ov109  |  profile @ 0x0218c638
 class Object214 : public StageEntity {
 public:
+	bool onHeapCreated();
+	void pendingDestroy();
+	bool _01();
 	static void *create();
 	inline Object214() {};
 	inline ~Object214() {};
