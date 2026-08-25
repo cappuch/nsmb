83a4a77cb07177d180e7c2c0aa576554d1ef797e
cappuch <mikusdevr@gmail.com>
2026-08-25 03:30:37 +0100

    tiny: match 3 fn in src/Bases/stubs/Object103.cpp (Object103::_21, Object103::onDestroy, Object103::pendingDestroy)

--- src/Bases/stubs/Object103.cpp
+++ src/Bases/stubs/Object103.cpp
@@ -7,3 +7,15 @@ void *Object103::create()
 
 // 0x0218dfa8
 ActorProfile Object103_Profile = { Object103::create, 103, 224, NULL /* TODO: 0x0218d250 */ };
+
+void Object103::_21() {
+
+}
+
+s32 Object103::onDestroy() {
+	return 1;
+}
+
+void Object103::pendingDestroy() {
+
+}
--- src/Bases/stubs/Object103.hpp
+++ src/Bases/stubs/Object103.hpp
@@ -6,6 +6,9 @@
 // MainProfileTable slot 103  |  ov121  |  profile @ 0x0218dfa8
 class Object103 : public StageEntity {
 public:
+	void pendingDestroy();
+	s32 onDestroy();
+	void _21();
 	Model _3f4;
 	u8 _pad0[0x28];
 	
