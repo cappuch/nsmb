216de81310bcbf4e7dcf5946f44759d786d4baf0
cappuch <mikusdevr@gmail.com>
2026-08-25 03:48:38 +0100

    tiny2: match 1 fn in src/Bases/stubs/Object165.cpp (func_ov010_020dcb28)

--- src/Bases/stubs/Object165.cpp
+++ src/Bases/stubs/Object165.cpp
@@ -7,3 +7,7 @@ void *Object165::create()
 
 // 0x02125dd8
 ActorProfile Object165_Profile = { Object165::create, 165, 204, NULL /* TODO: 0x020dc7c4 */ };
+
+extern "C" void func_ov010_020dcb28() {
+
+}
