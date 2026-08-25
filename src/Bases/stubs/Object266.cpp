4ae6e84ae5eebf4551d2bfb71d6ee9c2e9ee4913
cappuch <mikusdevr@gmail.com>
2026-08-25 03:30:43 +0100

    tiny: match 1 fn in src/Bases/stubs/Object266.cpp (Object266::onUpdate_0)

--- src/Bases/stubs/Object266.cpp
+++ src/Bases/stubs/Object266.cpp
@@ -7,3 +7,7 @@ void *Object266::create()
 
 // 0x02146ad8
 ActorProfile Object266_Profile = { Object266::create, 266, 156, NULL };
+
+bool Object266::onUpdate_0() {
+	return 1;
+}
--- src/Bases/stubs/Object266.hpp
+++ src/Bases/stubs/Object266.hpp
@@ -5,6 +5,7 @@
 // MainProfileTable slot 266  |  ov040  |  profile @ 0x02146ad8
 class Object266 : public StageEntity {
 public:
+	bool onUpdate_0();
 	static void *create();
 	inline Object266() {};
 	inline ~Object266() {};
