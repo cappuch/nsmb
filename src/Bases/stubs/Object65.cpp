86216492710bc56190fc396b486c396ab8a727c9
cappuch <mikusdevr@gmail.com>
2026-08-25 03:30:47 +0100

    tiny: match 2 fn in src/Bases/stubs/Object65.cpp (Object65::_31, Object65::pendingDestroy)

--- src/Bases/stubs/Object65.cpp
+++ src/Bases/stubs/Object65.cpp
@@ -7,3 +7,11 @@ void *Object65::create()
 
 // 0x0213f928
 ActorProfile Object65_Profile = { Object65::create, 65, 79, NULL /* TODO: 0x0213effc */ };
+
+void Object65::pendingDestroy() {
+
+}
+
+void Object65::_31() {
+
+}
--- src/Bases/stubs/Object65.hpp
+++ src/Bases/stubs/Object65.hpp
@@ -5,6 +5,8 @@
 // MainProfileTable slot 65  |  ov024  |  profile @ 0x0213f928
 class Object65 : public StageEntity {
 public:
+	void _31();
+	void pendingDestroy();
 	static void *create();
 	inline Object65() {};
 	inline ~Object65() {};
