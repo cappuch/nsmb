fc13bf62e9888339036a96be15a891f1283f121c
cappuch <mikusdevr@gmail.com>
2026-08-25 03:30:42 +0100

    tiny: match 5 fn in src/Bases/stubs/Object239.cpp (Object239::_22, Object239::_25, Object239::_27, Object239::_30, Object239::onStomped)

--- src/Bases/stubs/Object239.cpp
+++ src/Bases/stubs/Object239.cpp
@@ -7,3 +7,23 @@ void *Object239::create()
 
 // 0x021330e8
 ActorProfile Object239_Profile = { Object239::create, 239, 30, NULL /* TODO: 0x02132e4c */ };
+
+void Object239::_30() {
+
+}
+
+void Object239::_25() {
+
+}
+
+void Object239::_22() {
+
+}
+
+void Object239::_27() {
+
+}
+
+void Object239::onStomped() {
+
+}
--- src/Bases/stubs/Object239.hpp
+++ src/Bases/stubs/Object239.hpp
@@ -5,6 +5,11 @@
 // MainProfileTable slot 239  |  ov014  |  profile @ 0x021330e8
 class Object239 : public StageEntity {
 public:
+	void onStomped();
+	void _27();
+	void _22();
+	void _25();
+	void _30();
 	static void *create();
 	inline Object239() {};
 	inline ~Object239() {};
