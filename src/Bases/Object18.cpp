0383f2af3075b72911c0caa5aa861aa291460495
cappuch <mikusdevr@gmail.com>
2026-08-25 12:52:32 +0100

    decomp: match Object18 destroy cleanup

--- src/Bases/Object18.cpp
+++ src/Bases/Object18.cpp
@@ -1,5 +1,8 @@
 #include "Object18.hpp"
 
+extern u32 data_02085a84;
+extern "C" void func_ov052_021594c4();
+
 void *Object18::create()
 {
 	return new Object18();
@@ -7,6 +10,13 @@ void *Object18::create()
 
 s32 Object18::onRender() {
 
+}
+
+s32 Object18::onDestroy()
+{
+	if (BOOL(data_02085a84 != 0) != FALSE)
+		func_ov052_021594c4();
+	return true;
 }
 
 Object18::~Object18() {
