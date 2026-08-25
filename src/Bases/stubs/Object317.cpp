38a80b41d47fed175939ebb13db821e0f54c10f4
cappuch <mikusdevr@gmail.com>
2026-08-25 03:30:44 +0100

    tiny: match 2 fn in src/Bases/stubs/Object317.cpp (Object317::onDestroy, Object317::pendingDestroy)

--- src/Bases/stubs/Object317.cpp
+++ src/Bases/stubs/Object317.cpp
@@ -7,3 +7,11 @@ void *Object317::create()
 
 // 0x020e59a8
 ActorProfile Object317_Profile = { Object317::create, 317, 292, NULL /* TODO: 0x020cd478 */ };
+
+void Object317::pendingDestroy() {
+
+}
+
+s32 Object317::onDestroy() {
+	return 1;
+}
--- src/Bases/stubs/Object317.hpp
+++ src/Bases/stubs/Object317.hpp
@@ -5,6 +5,8 @@
 // MainProfileTable slot 317  |  ov008  |  profile @ 0x020e59a8
 class Object317 : public Actor {
 public:
+	s32 onDestroy();
+	void pendingDestroy();
 	static void *create();
 	inline Object317() {};
 	inline ~Object317() {};
