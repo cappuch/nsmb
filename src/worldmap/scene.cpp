502281a4c0dec51b1dcf83a670a93fc3c98436be
cappuch <mikusdevr@gmail.com>
2026-08-25 03:48:45 +0100

    tiny2: match 4 fn in src/worldmap/scene.cpp (func_ov008_020ce228, func_ov008_020cf790, func_ov008_020d153c, func_ov008_020d1540)

--- src/worldmap/scene.cpp
+++ src/worldmap/scene.cpp
@@ -1,1 +1,17 @@
 #include "scene.hpp"
+
+extern "C" void func_ov008_020ce228() {
+
+}
+
+extern "C" void func_ov008_020cf790() {
+
+}
+
+extern "C" void func_ov008_020d153c() {
+
+}
+
+extern "C" void func_ov008_020d1540() {
+
+}
