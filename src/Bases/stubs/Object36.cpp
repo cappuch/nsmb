897f32a597f91187e91ae77c5bc12e346eeb97a1
cappuch <mikusdevr@gmail.com>
2026-08-25 03:48:44 +0100

    tiny2: match 2 fn in src/Bases/stubs/Object36.cpp (func_ov012_0213085c, func_ov012_02130860)

--- src/Bases/stubs/Object36.cpp
+++ src/Bases/stubs/Object36.cpp
@@ -11,3 +11,11 @@ ActorProfile Object36_Profile = { Object36::create, 36, 49, NULL /* TODO: 0x0213
 void Object36::pendingDestroy() {
 
 }
+
+extern "C" void func_ov012_0213085c() {
+
+}
+
+extern "C" void func_ov012_02130860() {
+
+}
