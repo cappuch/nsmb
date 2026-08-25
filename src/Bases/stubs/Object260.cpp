cc59a6f9494e037c3f6e0d8b9208f82d83d84b83
cappuch <mikusdevr@gmail.com>
2026-08-25 03:30:42 +0100

    tiny: match 2 fn in src/Bases/stubs/Object260.cpp (Object260::damagePlayer, Object260::pendingDestroy)

--- src/Bases/stubs/Object260.cpp
+++ src/Bases/stubs/Object260.cpp
@@ -7,3 +7,11 @@ void *Object260::create()
 
 // 0x0216dec4
 ActorProfile Object260_Profile = { Object260::create, 260, 260, NULL /* TODO: 0x0215b69c */ };
+
+void Object260::damagePlayer(ActiveCollider *, PlayerBase *) {
+
+}
+
+void Object260::pendingDestroy() {
+
+}
--- src/Bases/stubs/Object260.hpp
+++ src/Bases/stubs/Object260.hpp
@@ -5,6 +5,8 @@
 // MainProfileTable slot 260  |  ov054  |  profile @ 0x0216dec4
 class Object260 : public StageEntity {
 public:
+	void pendingDestroy();
+	void damagePlayer(ActiveCollider *, PlayerBase *);
 	static void *create();
 	inline Object260() {};
 	inline ~Object260() {};
