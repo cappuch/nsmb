e23feb90c41689bc166f2a850e5abae3ec87b74f
cappuch <mikusdevr@gmail.com>
2026-08-25 03:48:42 +0100

    tiny2: match 2 fn in src/Bases/stubs/Object265.cpp (func_ov013_0213b2d4, func_ov013_0213b2d8)

--- src/Bases/stubs/Object265.cpp
+++ src/Bases/stubs/Object265.cpp
@@ -7,3 +7,11 @@ void *Object265::create()
 
 // 0x0213bd2c
 ActorProfile Object265_Profile = { Object265::create, 265, 155, NULL };
+
+extern "C" void func_ov013_0213b2d4() {
+
+}
+
+extern "C" void func_ov013_0213b2d8() {
+
+}
