cd8d2d925b8d22eff70021f1d0b3325226c90f8d
cappuch <mikusdevr@gmail.com>
2026-08-25 03:30:44 +0100

    tiny: match 2 fn in src/Bases/stubs/Object306.cpp (Object306::onDestroy, Object306::pendingDestroy)

--- src/Bases/stubs/Object306.cpp
+++ src/Bases/stubs/Object306.cpp
@@ -7,3 +7,11 @@ void *Object306::create()
 
 // 0x020dac58
 ActorProfile Object306_Profile = { Object306::create, 306, 297, NULL /* TODO: 0x020d5330 */ };
+
+void Object306::pendingDestroy() {
+
+}
+
+s32 Object306::onDestroy() {
+	return 1;
+}
--- src/Bases/stubs/Object306.hpp
+++ src/Bases/stubs/Object306.hpp
@@ -6,6 +6,8 @@
 // MainProfileTable slot 306  |  ov009  |  profile @ 0x020dac58
 class Object306 : public Actor {
 public:
+	s32 onDestroy();
+	void pendingDestroy();
 	ModelAnm _120;
 	u8 _pad0[0x1c];
 	static void *create();
