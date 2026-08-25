4a7c54f530ef686c827b0bc2251f80d1d9635e71
cappuch <mikusdevr@gmail.com>
2026-08-25 03:30:38 +0100

    tiny: match 2 fn in src/Bases/stubs/Object112.cpp (Object112::_21, Object112::onStomped)

--- src/Bases/stubs/Object112.cpp
+++ src/Bases/stubs/Object112.cpp
@@ -7,3 +7,11 @@ void *Object112::create()
 
 // 0x02148548
 ActorProfile Object112_Profile = { Object112::create, 112, 130, NULL /* TODO: 0x0214833c */ };
+
+void Object112::_21() {
+
+}
+
+void Object112::onStomped() {
+
+}
--- src/Bases/stubs/Object112.hpp
+++ src/Bases/stubs/Object112.hpp
@@ -5,6 +5,8 @@
 // MainProfileTable slot 112  |  ov046  |  profile @ 0x02148548
 class Object112 : public StageEntity {
 public:
+	void onStomped();
+	void _21();
 	static void *create();
 	inline Object112() {};
 	inline ~Object112() {};
