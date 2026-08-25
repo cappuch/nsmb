3a2f6638bc4b53d187658a8c07bd4067796a3234
cappuch <mikusdevr@gmail.com>
2026-08-25 03:30:44 +0100

    tiny: match 1 fn in src/Bases/stubs/Object315.cpp (Object315::onDestroy)

--- src/Bases/stubs/Object315.cpp
+++ src/Bases/stubs/Object315.cpp
@@ -7,3 +7,7 @@ void *Object315::create()
 
 // 0x020ccd30
 ActorProfile Object315_Profile = { Object315::create, 315, 306, NULL };
+
+s32 Object315::onDestroy() {
+	return 1;
+}
--- src/Bases/stubs/Object315.hpp
+++ src/Bases/stubs/Object315.hpp
@@ -5,6 +5,7 @@
 // MainProfileTable slot 315  |  ov004  |  profile @ 0x020ccd30
 class Object315 : public StageEntity {
 public:
+	s32 onDestroy();
 	static void *create();
 	inline Object315() {};
 	inline ~Object315() {};
