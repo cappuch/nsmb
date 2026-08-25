0ff35810ec8da5c2e8c48977a97e77a5b7cbc220
cappuch <mikusdevr@gmail.com>
2026-08-25 03:30:45 +0100

    tiny: match 6 fn in src/Bases/stubs/Object345.cpp (Object345::_14, Object345::onUpdate_1, Object345::onUpdate_6, Object345::onUpdate_8, Object345::onUpdate_defeated, Object345::pendingDestroy)

--- src/Bases/stubs/Object345.cpp
+++ src/Bases/stubs/Object345.cpp
@@ -7,3 +7,27 @@ void *Object345::create()
 
 // 0x020feb50
 ObjectProfile Object345_Profile = { Object345::create, 345, 345 };
+
+bool Object345::onUpdate_1() {
+	return 1;
+}
+
+bool Object345::onUpdate_defeated() {
+	return 1;
+}
+
+bool Object345::onUpdate_6() {
+	return 0;
+}
+
+void Object345::pendingDestroy() {
+
+}
+
+bool Object345::onUpdate_8() {
+
+}
+
+bool Object345::_14() {
+
+}
--- src/Bases/stubs/Object345.hpp
+++ src/Bases/stubs/Object345.hpp
@@ -5,6 +5,12 @@
 // MainProfileTable slot 345  |  ov128  |  profile @ 0x020feb50
 class Object345 : public StageEntity {
 public:
+	bool _14();
+	bool onUpdate_8();
+	void pendingDestroy();
+	bool onUpdate_6();
+	bool onUpdate_defeated();
+	bool onUpdate_1();
 	static void *create();
 	inline Object345() {};
 	inline ~Object345() {};
