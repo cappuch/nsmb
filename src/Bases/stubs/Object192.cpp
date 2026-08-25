7dc454c92aa620a15ede7643b45d7c7830305ca4
cappuch <mikusdevr@gmail.com>
2026-08-25 03:48:39 +0100

    tiny2: match 1 fn in src/Bases/stubs/Object192.cpp (func_ov049_02147ea8)

--- src/Bases/stubs/Object192.cpp
+++ src/Bases/stubs/Object192.cpp
@@ -7,3 +7,7 @@ void *Object192::create()
 
 // 0x02147ff0
 ActorProfile Object192_Profile = { Object192::create, 192, 238, NULL /* TODO: 0x02147ec4 */ };
+
+extern "C" void func_ov049_02147ea8() {
+
+}
