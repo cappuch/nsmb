23ce95225f837510ab8f4a90c71a72bb97673d21
cappuch <mikusdevr@gmail.com>
2026-08-25 03:30:42 +0100

    tiny: match 2 fn in src/Bases/stubs/Object259.cpp (Object259::_23, Object259::_30)

--- src/Bases/stubs/Object259.cpp
+++ src/Bases/stubs/Object259.cpp
@@ -7,3 +7,11 @@ void *Object259::create()
 
 // 0x02178438
 ActorProfile Object259_Profile = { Object259::create, 259, 144, NULL /* TODO: 0x021780e4 */ };
+
+void Object259::_23() {
+
+}
+
+void Object259::_30() {
+
+}
--- src/Bases/stubs/Object259.hpp
+++ src/Bases/stubs/Object259.hpp
@@ -5,6 +5,8 @@
 // MainProfileTable slot 259  |  ov069  |  profile @ 0x02178438
 class Object259 : public StageEntity {
 public:
+	void _30();
+	void _23();
 	static void *create();
 	inline Object259() {};
 	inline ~Object259() {};
