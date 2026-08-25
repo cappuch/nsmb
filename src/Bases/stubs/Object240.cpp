13d053c89b50875b7a868be527d7e7e4dc3bd0f1
cappuch <mikusdevr@gmail.com>
2026-08-25 03:48:41 +0100

    tiny2: match 2 fn in src/Bases/stubs/Object240.cpp (func_ov010_020cf698, func_ov010_020d052c)

--- src/Bases/stubs/Object240.cpp
+++ src/Bases/stubs/Object240.cpp
@@ -7,3 +7,11 @@ void *Object240::create()
 
 // 0x02124014
 ActorProfile Object240_Profile = { Object240::create, 240, 43, NULL };
+
+extern "C" void func_ov010_020cf698() {
+
+}
+
+extern "C" void func_ov010_020d052c() {
+
+}
