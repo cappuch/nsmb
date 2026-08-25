723d72de854699478b0f9d18f3100622e2c2a278
cappuch <mikusdevr@gmail.com>
2026-08-25 03:48:42 +0100

    tiny2: match 7 fn in src/Bases/stubs/Object314.cpp (data_ov008_020f14f0, func_ov008_020e0760, func_ov008_020e090c, func_ov008_020e0dec, func_ov008_020e0f98, func_ov008_020e1120, func_ov008_020e1124)

--- src/Bases/stubs/Object314.cpp
+++ src/Bases/stubs/Object314.cpp
@@ -7,3 +7,31 @@ void *Object314::create()
 
 // 0x020ee2c8
 ActorProfile Object314_Profile = { Object314::create, 314, 305, NULL };
+
+extern "C" void * data_ov008_020f14f0() {
+	return &data_ov008_020f14f0;
+}
+
+extern "C" int func_ov008_020e0760() {
+	return 1;
+}
+
+extern "C" int func_ov008_020e090c() {
+	return 1;
+}
+
+extern "C" int func_ov008_020e0dec() {
+	return 1;
+}
+
+extern "C" int func_ov008_020e0f98() {
+	return 1;
+}
+
+extern "C" void func_ov008_020e1120() {
+
+}
+
+extern "C" int func_ov008_020e1124() {
+	return 1;
+}
